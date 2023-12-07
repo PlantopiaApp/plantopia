13ed051e96ae7207cd00949b05541c73669a092b
Zeena <s92064676@ousl.lk>
2023-12-07 22:22:52 +0530

    12th Dec

--- app/src/main/java/com/plants/plantopia/PlantIdentificationResponse.java
+++ app/src/main/java/com/plants/plantopia/PlantIdentificationResponse.java
@@ -5,7 +5,7 @@ import com.google.gson.annotations.SerializedName;
 import java.util.List;
 
 public class PlantIdentificationResponse {
-
+//drcfvgbhj
     @SerializedName("suggestions")
     private List<PlantSuggestion> suggestions;
 
